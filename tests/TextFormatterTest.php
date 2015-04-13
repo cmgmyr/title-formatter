17b39d4a54acc39b3ba60233c040345bd63b1b0c
Chris Gmyr <cmgmyr@gmail.com>
2015-04-13 15:00:49 -0400

    updated tests

--- tests/TextFormatterTest.php
+++ tests/TextFormatterTest.php
@@ -4,93 +4,71 @@ use Cmgmyr\TextFormatter\TextFormatter;
 
 class TextFormatterTest extends TestCase
 {
-    /** @test */
-    public function it_should_format_single_sentence()
-    {
-        $startingTitle = "is this sh*t is a test";
-        $endingTitle = TextFormatter::titleCase($startingTitle);
-        $correctTitle = "Is This Sh*t is a Test";
-
-        $this->assertEquals($correctTitle, $endingTitle);
-    }
-
-    /** @test */
-    public function it_should_format_multiple_sentences()
-    {
-        $startingTitle = "is this sh*t is a test. for sure!!!";
-        $endingTitle = TextFormatter::titleCase($startingTitle);
-        $correctTitle = "Is This Sh*t is a Test. For Sure!!!";
-
-        $this->assertEquals($correctTitle, $endingTitle);
-    }
-
-    /** @test */
-    public function it_should_format_multiple_sentences_with_brackets()
-    {
-        $startingTitle = "is this sh*t is a test. for sure. [yikes as example]";
-        $endingTitle = TextFormatter::titleCase($startingTitle);
-        $correctTitle = "Is This Sh*t is a Test. For Sure. [Yikes as Example]";
-
-        $this->assertEquals($correctTitle, $endingTitle);
-    }
-
-    /** @test */
-    public function it_should_format_dashed_word()
-    {
-        $startingTitle = "this should be a super-awesome post!";
-        $endingTitle = TextFormatter::titleCase($startingTitle);
-        $correctTitle = "This Should Be a Super-Awesome Post!";
-
-        $this->assertEquals($correctTitle, $endingTitle);
-    }
-
-    /** @test */
-    public function it_should_format_multiple_spaces()
-    {
-        $startingTitle = "this    should be interesting";
-        $endingTitle = TextFormatter::titleCase($startingTitle);
-        $correctTitle = "This Should Be Interesting";
-
-        $this->assertEquals($correctTitle, $endingTitle);
-    }
-
-    /** @test */
-    public function it_should_capitalize_the_first_word()
-    {
-        $startingTitle = "very simple sentence";
-        $endingTitle = TextFormatter::titleCase($startingTitle);
-        $correctTitle = "Very Simple Sentence";
-
-        $this->assertEquals($correctTitle, $endingTitle);
-    }
+    protected $tests = [
+        [
+            // tests single sentence with mid-word special character
+            'title' => 'this sh*t is a test',
+            'correct' => 'This Sh*t is a Test'
+        ],
+        [
+            // tests multiple sentences
+            'title' => 'is this sh*t is a test. for sure!!!',
+            'correct' => 'Is This Sh*t is a Test. For Sure!!!'
+        ],
+        [
+            // tests sentences in brackets
+            'title' => 'is this sh*t is a test. for sure. [yikes as example]',
+            'correct' => 'Is This Sh*t is a Test. For Sure. [Yikes as Example]'
+        ],
+        [
+            // tests dashed words
+            'title' => 'this should be a super-awesome post! cool? not-so-much!',
+            'correct' => 'This Should Be a Super-Awesome Post! Cool? Not-So-Much!'
+        ],
+        [
+            // tests sentence with extra spaces
+            'title' => '   this    should be interesting    ',
+            'correct' => 'This Should Be Interesting'
+        ],
+        [
+            // tests simple sentence
+            'title' => 'very simple sentence',
+            'correct' => 'Very Simple Sentence'
+        ],
+        [
+            // tests to make sure last word is capitalized
+            'title' => 'very simple sentence of',
+            'correct' => 'Very Simple Sentence Of'
+        ],
+        [
+            // tests words that have capital letters, they should be ignored
+            'title' => 'i think eBay is the greatest site! also, McCormick has the best spices!',
+            'correct' => 'I Think eBay is the Greatest Site! Also, McCormick Has the Best Spices!'
+        ],
+        [
+            // tests words with multiple punctuation prefixes
+            'title' => 'this post is $$$money',
+            'correct' => 'This Post is $$$Money'
+        ],
+        [
+            // tests for all CAPS words, they should be ignored
+            'title' => 'i really like playing with LEGOS, they are a lot of fun!',
+            'correct' => 'I Really Like Playing With LEGOS, They Are a Lot of Fun!'
+        ],
+        [
+            // tests for a bug in first words with apostrophes, should not capitalize second letter
+            'title' => 'it\'s really something, isn\'t it?',
+            'correct' => 'It\'s Really Something, Isn\'t It?'
+        ],
+    ];
 
     /** @test */
-    public function it_should_capitalize_the_last_word()
+    public function it_should_format_titles_correctly()
     {
-        $startingTitle = "very simple sentence of";
-        $endingTitle = TextFormatter::titleCase($startingTitle);
-        $correctTitle = "Very Simple Sentence Of";
+        foreach ($this->tests as $test) {
+            $newTitle = TextFormatter::titleCase($test['title']);
 
-        $this->assertEquals($correctTitle, $endingTitle);
-    }
-
-    /** @test */
-    public function it_should_ignore_words_that_have_capital_letters()
-    {
-        $startingTitle = "i think eBay is the greatest site! also, McCormick has the best spices!";
-        $endingTitle = TextFormatter::titleCase($startingTitle);
-        $correctTitle = "I Think eBay is the Greatest Site! Also, McCormick Has the Best Spices!";
-
-        $this->assertEquals($correctTitle, $endingTitle);
-    }
-
-    /** @test */
-    public function it_should_capitalize_after_multiple_punctuation()
-    {
-        $startingTitle = 'this post is $$$money';
-        $endingTitle = TextFormatter::titleCase($startingTitle);
-        $correctTitle = 'This Post is $$$Money';
-
-        $this->assertEquals($correctTitle, $endingTitle);
+            $this->assertEquals($test['correct'], $newTitle);
+        }
     }
 }
